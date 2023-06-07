ad1020bed740a217b1ffdee60be74a7669a684c9
h80024 <38033593+h80024@users.noreply.github.com>
2023-06-07 16:48:49 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://drpy.site/js1");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://freed.yuanhsing.cf/TVBox/meowcf.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
